517de9f4036d85f0ed5f72203432300cbf48be47
elizarov <elizarov@gmail.com>
2011-03-01 21:17:28 +0300

    outlier filtering added

--- src/org/vsegda/util/DataRequest.java
+++ src/org/vsegda/util/DataRequest.java
@@ -1,6 +1,5 @@
 package org.vsegda.util;
 
-import org.apache.commons.beanutils.BeanUtils;
 import org.vsegda.data.DataItem;
 import org.vsegda.data.DataStream;
 
@@ -18,6 +17,7 @@ public class DataRequest {
 	private IdList id;
 	private int first;
 	private int last = 1000; // last 1000 items by default
+    private double filter = 5; // 5 sigmas by default
 
 	public DataRequest(ServletRequest req) {
         RequestUtil.populate(this, req);
@@ -34,7 +34,7 @@ public class DataRequest {
 
     @SuppressWarnings({"unchecked"})
     public Collection<DataItem> query(PersistenceManager pm) {
-        Collection<DataItem> items = new ArrayList<DataItem>();
+        List<DataItem> items = new ArrayList<DataItem>();
         if (id == null) {
             Query query = pm.newQuery(DataStream.class);
             query.setOrdering("streamId asc");
@@ -48,11 +48,55 @@ public class DataRequest {
                 query.declareParameters("int id");
                 query.setOrdering("timeMillis desc");
                 query.setRange(first, first + last);
+                int s0 = items.size();
                 items.addAll((Collection<DataItem>)query.execute(id));
+                filter(items.subList(s0, items.size()));
             }
         }
         return items;
     }
+
+    private void filter(List<DataItem> items) {
+        if (filter <= 0)
+            return;
+        for (int repeat = 0; repeat < 20; repeat++) {
+            int n = items.size() - 1;
+            if (n < 2)
+                return;
+            double sum = 0;
+            double sum2 = 0;
+            for (int i = 0; i < n; i++) {
+                double d = diff(items, i);
+                sum += d;
+                sum2 += d * d;
+            }
+            double m = sum / n;
+            double s2 = sum2 / (n - 1) - sum * sum / (n * n - n);
+            if (s2 <= 0)
+                return;
+            double limit = Math.sqrt(s2) * filter;
+            int j = 0;
+            for (int i = 0; i < items.size(); i++) {
+                if (absError(items, i, m) <= limit || absError(items, i - 1, m) <= limit) {
+                    items.set(j++, items.get(i));
+                }
+            }
+            if (j == items.size())
+                return;
+            items.subList(j, items.size()).clear();
+        }
+    }
+
+    private double absError(List<DataItem> items, int i, double m) {
+        if (i < 0 || i >= items.size() - 1)
+            return Double.POSITIVE_INFINITY;
+        return Math.abs(m - Math.abs(diff(items, i)));
+    }
+
+    private double diff(List<DataItem> items, int i) {
+        return items.get(i).getValue() - items.get(i + 1).getValue();
+    }
+
 
     public IdList getId() {
         return id;
@@ -76,5 +120,13 @@ public class DataRequest {
 
     public void setLast(int last) {
         this.last = last;
+    }
+
+    public double getFilter() {
+        return filter;
+    }
+
+    public void setFilter(double filter) {
+        this.filter = filter;
     }
 }
