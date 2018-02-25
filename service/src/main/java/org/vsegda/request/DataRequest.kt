37554aa3886e829193b6170cff0d29ef06e7713d
Roman Elizarov <elizarov@gmail.com>
2018-02-25 13:16:46 +0300

    log fixed

--- service/src/main/java/org/vsegda/request/DataRequest.kt
+++ service/src/main/java/org/vsegda/request/DataRequest.kt
@@ -58,7 +58,7 @@ class DataRequest(req: HttpServletRequest? = null) : AbstractRequest() {
     }
 
     fun queryMap(): Map<DataStream, List<DataItem>> =
-        logged("Data query $this", around = true, result = { "${it.size} items" }) {
+        logged("Data query $this", around = true, result = { "${it.values.sumBy { it.size } } items" }) {
             val id = this.id
             if (id == null) {
                 DataStreamService.dataStreams
